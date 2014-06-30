6d8a2173249c794a014d0467f8dcbf349b81d26d
Fitz Elliott <felliott@fiskur.org>
2014-06-30 11:41:48 -0400

    inital commit

---
+++ lib/WebApp/Helpers/MimeTypes.pm
@@ -0,0 +1,39 @@
+package WebApp::Helpers::MimeTypes;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebApp::Helpers::MimeTypes - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebApp::Helpers::MimeTypes;
+
+=head1 DESCRIPTION
+
+WebApp::Helpers::MimeTypes is ...
+
+=head1 LICENSE
+
+Copyright (C) Fitz Elliott.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Fitz Elliott E<lt>felliott@fiskur.orgE<gt>
+
+=cut
+
